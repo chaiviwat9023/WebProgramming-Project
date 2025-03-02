ff9e3c54322003f8bfe83e5a77a972875773b2c4
66070251 <141380936+chaiviwat9023@users.noreply.github.com>
2025-03-03 02:20:17 +0700

    show history

--- RentRoom/routes/history.js
+++ RentRoom/routes/history.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../config/database"); // นำเข้า database.js
 
-router.get("/", (req, res) => {
-    res.render("history");
+router.get('/', (req, res) => {
+    const sql = `
+        SELECT reservation_id, reservation_date, room_id, 
+            CASE status 
+                WHEN 'pending' THEN 'รอดำเนินการ' 
+                WHEN 'approved' THEN 'อนุมัติ' 
+                WHEN 'rejected' THEN 'ปฏิเสธ' 
+                WHEN 'canceled' THEN 'ยกเลิก' 
+            END AS status
+        FROM reservations;
+    `;
+
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.render('history', { data: rows });
+    });
 });
 
 module.exports = router;
